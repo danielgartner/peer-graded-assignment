f4082ae611e546a3d5bae0e667e0982f6527e094
danielgartner <danielgartner@gmail.com>
2020-04-02 21:34:42 -0400

    makefile.sh

--- makefile.sh
+++ makefile.sh
@@ -1,3 +1,3 @@
 echo "## Title of project: guessinggame.sh" > README.md
-echo "### makefile was run on: date >> README.md
-echo "#### Number of lines of code: " wc -l guessinggame.sh | egrep -o "[0-9]+" >> README.md
+echo "### makefile was run on: date" >> README.md
+echo " Number of lines of code: " wc -l guessinggame.sh | egrep -o "[0-9]+" >> README.md
